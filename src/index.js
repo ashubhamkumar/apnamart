dbca84e25453a0ab33428eab24d4746e6c446065
shubham kumar <devshubhamyadav@gmail.com>
2022-06-10 19:23:47 +0530

    setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
+import { AuthContextProvider } from "./store/authContext";
+import { UserContextProvider } from "./store/userContext";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
-
-
-
