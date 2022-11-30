f7e95b365958220ba9b5b72ea913bfb6e431f60d
shubham kumar <devshubhamyadav@gmail.com>
2022-11-30 10:27:06 +0000

    demo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -122,16 +122,15 @@ const Header = () => {
   };
   return (
     <>
-      <div className="bg-indigo-600">
-        <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+      <div className="bg-indigo-400">
+        <div className="mx-auto  py-2 px-2 sm:px-4">
           <div className="flex flex-wrap items-center justify-between">
             <div className="flex w-0 flex-1 items-center">
               <span className="flex rounded-lg bg-indigo-800 p-2">
-                <BellIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                <BellIcon className="h-5 w-5 text-white" aria-hidden="true" />
               </span>
               <p className="ml-3 truncate font-medium text-white">
-                <span className="md:hidden">We announced a new product!</span>
-                <span className="hidden md:inline">
+                <span className=" whitespace-nowrap">
                   Big news! We're excited to announce product demo here.
                 </span>
               </p>
@@ -141,7 +140,7 @@ const Header = () => {
                 href="https://youtu.be/KTmhVbybIcs"
                 target={"_blank"}
                 rel="noopener noreferrer"
-                className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50"
+                className="flex items-center justify-center rounded-md border whitespace-nowrap border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50"
               >
                 View Demo
               </a>
