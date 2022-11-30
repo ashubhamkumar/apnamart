897c23c24460f2197e03a452b140cbe130995c18
shubham kumar <devshubhamyadav@gmail.com>
2022-11-30 10:11:38 +0000

    demo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -139,6 +139,7 @@ const Header = () => {
             <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
               <Link
                 to="https://youtu.be/KTmhVbybIcs"
+                target={"_blank"}
                 className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50"
               >
                 View Demo
